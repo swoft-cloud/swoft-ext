48e04cf55ff77af3039419c926d5a5c003408d06
stelin <794774870@qq.com>
2019-08-01 20:13:02 +0800

    Add crontab and whoops

---
+++ src/crontab/test/unit/CrontabTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+
+namespace SwoftTest\Crontab\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CrontabTest
+ *
+ * @since 2.0
+ */
+class CrontabTest extends TestCase
+{
+    public function testIndex()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ src/whoops/test/unit/WhoopsTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+
+namespace SwoftTest\Whoops\Testing;
+
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class WhoopsTest
+ *
+ * @since 2.0
+ */
+class WhoopsTest extends TestCase
+{
+    public function testIndex()
+    {
+        $this->assertTrue(true);
+    }
+}
