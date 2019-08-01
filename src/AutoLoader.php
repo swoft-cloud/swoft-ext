fc49a069f499c0620c33553fc0b9e8a12a8c75ac
stelin <794774870@qq.com>
2019-08-01 19:49:59 +0800

    Squashed 'src/whoops/' content from commit 30ce135
    git-subtree-dir: src/whoops
    git-subtree-split: 30ce1359d54f5a8573d6249f47c03049d5ac4011

---
+++ src/AutoLoader.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Swoft\Whoops;
+
+use Swoft\Helper\ComposerJSON;
+use Swoft\SwoftComponent;
+use function dirname;
+
+/**
+ * Class AutoLoader
+ *
+ * @since 2.0
+ */
+class AutoLoader extends SwoftComponent
+{
+    /**
+     * @return array
+     */
+    public function getPrefixDirs(): array
+    {
+        return [
+            __NAMESPACE__ => __DIR__,
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function metadata(): array
+    {
+        $jsonFile = dirname(__DIR__) . '/composer.json';
+
+        return ComposerJSON::open($jsonFile)->getMetadata();
+    }
+}
