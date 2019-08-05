613c01ba2c4b81fc9985d67db06449f05c6a9d22
stelin <794774870@qq.com>
2019-08-05 20:38:34 +0800

    add

--- src/swoole-dashboard/src/Logic/SwooleDashboardLogic.php
+++ src/swoole-dashboard/src/Logic/SwooleDashboardLogic.php
@@ -6,7 +6,7 @@ namespace Swoft\Swoole\Dashboard\Logic;
 
 use Swoft\Bean\Annotation\Mapping\Bean;
 use Swoft\Bean\Annotation\Mapping\Inject;
-use Swoft\swoole\Dashboard\SwooleDashboard;
+use Swoft\Swoole\Dashboard\SwooleDashboard;
 
 /**
  * Class SwooleDashboardLogic
